a51f19159132c3c61859e731c2bd32a426ced64f
Yaeko Ogino <yaekoogino@gmail.com>
2017-12-03 19:19:21 -0300

    no inicializa el mockadapter si no es necesario

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-var mock = new MockAdapter(axios, {delayResponse:200});
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 window.Vue = Vue
 window.axios = axios
-window.mock = mock
+window.MockAdapter = MockAdapter
 window.app = new Vue({
   el: '#app',
   render: h => h(App)
